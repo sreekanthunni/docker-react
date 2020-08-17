4b6722e96abf02916b5a02c00a1c07704e996094
Sreekanth Unnikrishnan <sreekanthetl@gmal.com>
2020-08-17 12:59:49 -0400

    Changed to check AWS beanstalk error

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was Changed 
+          I was Changed to check AWS beanstalk error
         </p>
         <a
           className="App-link"
